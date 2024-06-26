6ea2bec2a6a12cc5f63f13a2295d6bb7ef1a589e
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2024-06-25 19:29:44 -0500

    near completion

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,146 @@
 'use strict';
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
+mongoose.connect(process.env.MONGO_URI);
+const Project = mongoose.model('Project', new mongoose.Schema({
+  project_title: String,
+  issues: Array
+}));
+const Issue = {
+  issue_title: String,
+  issue_text: String,
+  created_on: Date,
+  updated_on: Date,
+  created_by: String,
+  assigned_to: String,
+  status_text: String,
+  open: Boolean
+}
+
+async function getOrCreateProject(projectName) {
+  let project = await Project.findOne({project_title: projectName});
+  if (!project) {
+    project = await new Project({
+      project_title: projectName,
+      issues: []
+    }).save();
+  }
+  return project;
+}
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async (req, res) => {
       let project = req.params.project;
-      
+      if (!project) {
+        res.send('Please enter a project name.');
+        return;
+      }
+      let projectDB = await getOrCreateProject(project);
+      const queries = req.query;
+      const filteredIssues = projectDB.issues.filter(el => {
+        if (queries.hasOwnProperty('issue_title') && el.issue_title !== queries.issue_title) return false;
+        if (queries.hasOwnProperty('issue_text') && el.issue_text !== queries.issue_text) return false;
+        if (queries.hasOwnProperty('created_on') && el.created_on !== queries.created_on) return false;
+        if (queries.hasOwnProperty('updated_on') && el.updated_on !== queries.updated_on) return false;
+        if (queries.hasOwnProperty('created_by') && el.created_by !== queries.created_by) return false;
+        if (queries.hasOwnProperty('assigned_to') && el.assigned_to !== queries.assigned_to) return false;
+        if (queries.hasOwnProperty('status_text') && el.status_text !== queries.status_text) return false;
+        if (queries.hasOwnProperty('open') && el.open.toString() !== queries.open) return false;
+        return true;
+      });
+      res.json(filteredIssues);
     })
     
-    .post(function (req, res){
+    .post(async (req, res) => {
       let project = req.params.project;
-      
+      if (!project) {
+        res.send('Please provide a project name.');
+        return;
+      }
+      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+      if (!issue_title || !issue_text || !created_by) {
+        res.send('issue title, text, and created_by fields are required.');
+        return;
+      }
+      let projectDB = await getOrCreateProject(project);
+      const issue = {
+        _id: new ObjectId(),
+        issue_title: issue_title,
+        issue_text: issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: created_by,
+        assigned_to: assigned_to || "",
+        status_text: status_text || "",
+        open: true
+      }
+      projectDB.issues.push(issue);
+      await projectDB.save();
+      res.json(issue);
     })
     
-    .put(function (req, res){
+    .put(async (req, res) => {
       let project = req.params.project;
-      
+      if (!project) {
+        res.send('Please provide a project name.');
+        return;
+      }
+      const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
+      if (!_id) {
+        res.send('_id field is required.');
+        return;
+      }
+      let projectDB = await getOrCreateProject(project);
+      const issue = projectDB.issues.find(el => el._id.toString() === _id);
+      if (!issue) {
+        res.send(`The issue _id provided does not match an existing project issue.`);
+        return;
+      }
+      const issueIndex = projectDB.issues.indexOf(issue);
+      const updatedIssue = {
+        _id: issue._id,
+        issue_title: issue_title || issue.issue_title,
+        issue_text: issue_text || issue.issue_text,
+        created_on: issue.created_on,
+        updated_on: new Date(),
+        created_by: created_by || issue.created_by,
+        assigned_to: assigned_to || issue.assigned_to,
+        status_text: status_text || issue.status_text,
+        open: open || issue.open
+      }
+      projectDB.issues[issueIndex] = updatedIssue;
+      await projectDB.save();
+      res.json(updatedIssue);
     })
     
-    .delete(function (req, res){
+    .delete(async (req, res) => {
       let project = req.params.project;
-      
+      if (!project) {
+        res.send('Please provide a project name.');
+        return;
+      }
+      const _id = req.body._id;
+      if (!_id) {
+        res.send('Please provide an issue _id');
+        return;
+      }
+      let projectDB = await getOrCreateProject(project);
+      const issue = projectDB.issues.find(el => el._id.toString() === _id);
+      if (!issue) {
+        res.send('The issue _id does not match an exist project issue.');
+        return;
+      }
+      const filteredIssues = projectDB.issues.filter(el => el._id.toString() !== _id);
+      projectDB.issues = filteredIssues;
+      await projectDB.save();
+      res.json({
+        result: 'successfully deleted',
+        _id
+      })
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,210 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  let testIds = [];
+
+  this.timeout(10000);
+  test('create issue with every field', done => {
+    chai.request(server)
+      .keepOpen()
+      .post('/api/issues/testing')
+      .send({
+        "issue_title": "post request test",
+        "issue_text": "this is a test with all fields",
+        "created_by": "chai testing suite",
+        "assigned_to": "testers",
+        "status_text": "in progress"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isNotNull(res.body._id);
+        assert.isNotNull(res.body.created_on);
+        assert.isNotNull(res.body.updated_on);
+        assert.equal(res.body.issue_title, 'post request test');
+        assert.equal(res.body.issue_text, 'this is a test with all fields');
+        assert.equal(res.body.created_by, 'chai testing suite');
+        assert.equal(res.body.assigned_to, 'testers');
+        assert.equal(res.body.status_text, 'in progress');
+        assert.isTrue(res.body.open);
+        testIds.push(res.body._id);
+        done();
+      });
+  });
+  test('create isssues with only required fields', done => {
+    chai.request(server)
+      .keepOpen()
+      .post('/api/issues/testing')
+      .send({
+        "issue_title": "post request test 2",
+        "issue_text": "this is a test with only required fields",
+        "created_by": "chai testing suite",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isNotNull(res.body._id);
+        assert.isNotNull(res.body.created_on);
+        assert.isNotNull(res.body.updated_on);
+        assert.equal(res.body.issue_title, 'post request test 2');
+        assert.equal(res.body.issue_text, 'this is a test with only required fields');
+        assert.equal(res.body.created_by, 'chai testing suite');
+        assert.isEmpty(res.body.assigned_to);
+        assert.isEmpty(res.body.status_text);
+        assert.isTrue(res.body.open);
+        testIds.push(res.body._id);
+        done();
+      });
+  });
+  test('create isssue with missing required fields', done => {
+    chai.request(server)
+      .keepOpen()
+      .post('/api/issues/testing')
+      .send({
+        "asssigned_to": "testers"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'issue title, text, and created_by fields are required.');
+        done();
+      });
+  });
+  test('view issues on a project', done => {
+    chai.request(server)
+      .keepOpen()
+      .get('/api/issues/testing')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        done();
+      });
+  });
+  test('view issues on a project with one filter', done => {
+    chai.request(server)
+      .keepOpen()
+      .get('/api/issues/testing?status_text=in%20progress')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isAtLeast(res.body.length, 1);
+        done();
+      });
+  });
+  test('view issues on a project with multiple filters', done => {
+    chai.request(server)
+      .keepOpen()
+      .get('/api/issues/testing?status_text=in%20progress&open=true')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isAtLeast(res.body.length, 1);
+        done();
+      });
+  });
+  test('update one field on an issue', done => {
+    chai.request(server)
+      .keepOpen()
+      .put('/api/issues/testing')
+      .send({
+        _id: testIds[0],
+        issue_text: 'this issue has been updated!'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body._id, testIds[0]);
+        assert.equal(res.body.issue_text, 'this issue has been updated!');
+        done();
+      });
+  });
+  test('update multiple fields on an issue', done => {
+    chai.request(server)
+      .keepOpen()
+      .put('/api/issues/testing')
+      .send({
+        _id: testIds[0],
+        issue_title: 'updated issue',
+        issue_text: 'this issue has been updated again!'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body._id, testIds[0]);
+        assert.equal(res.body.issue_title, 'updated issue');
+        assert.equal(res.body.issue_text, 'this issue has been updated again!');
+        done();
+      });
+  });
+  test('update an issue with missing _id', done => {
+    chai.request(server)
+      .keepOpen()
+      .put('/api/issues/testing')
+      .send({
+        issue_text: 'trying to update this issue!'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '_id field is required.');
+        done();
+      });
+  });
+  test('update an issue with no fields', done => {
+    chai.request(server)
+      .keepOpen()
+      .put('/api/issues/testing')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '_id field is required.');
+        done();
+      });
+  });
+  test('update an issue with an invalid _id', done => {
+    chai.request(server)
+      .keepOpen()
+      .put('/api/issues/testing')
+      .send({
+        _id: 'fdsajfie3jfjis',
+        issue_text: 'trying to update this issue!'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'The issue _id provided does not match an existing project issue.');
+        done();
+      });
+  });
+  test('delete an issue', done => {
+    chai.request(server)
+      .keepOpen()
+      .delete('/api/issues/testing')
+      .send({
+        _id: testIds[1]
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.result, 'successfully deleted');
+        assert.equal(res.body._id, testIds[1]);
+        done();
+      });
+  });
+  test('delete an issue with an invalid _id', done => {
+    chai.request(server)
+      .keepOpen()
+      .delete('/api/issues/testing')
+      .send({
+        _id: 'j3u1983u4rdsjkap'
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'The issue _id does not match an exist project issue.');
+        done();
+      });
+  });
+  test('delete an issue with a missing _id', done => {
+    chai.request(server)
+      .keepOpen()
+      .delete('/api/issues/testing')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'Please provide an issue _id');
+        done();
+      });
+  });
 });
